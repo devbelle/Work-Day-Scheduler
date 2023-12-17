85c71cac5d1ef85137b09ae7ea63baea1d9bce61
Devin <dbelle722@gmail.com>
2023-12-17 16:42:30 -0500

    Update Changes

--- script.js
+++ script.js
@@ -48,12 +48,12 @@ $("#currentDay").text(today);
   plannerDay.push(dataPlanner);
 }
 
-//Create function for settting items though local storage
-//Create function for getting items from local storage
+//checking for previous entries in the local storage to pull first
+var taskEvents = JSON.parse(localStorage.getItem("dayPlanner"));
+if (taskEvents) {
+  plannerDay = taskEvents;
+}
 
-
-//Create function that pulls more time slot entries. Will need to create html elements in javascript.
-//Will need to link created elements to container element in html
 //for creating each row. Use forEach to create my div tages and place the necessary selector from the plannerDay array
 plannerDay.forEach(function(timeSlot, index) {
   //the hour slot for each selection
@@ -80,38 +80,39 @@ plannerDay.forEach(function(timeSlot, index) {
 	$(".container").append(row);
 });
 
+
 //Create a function to select color of past, present, and future in CSS to html in javascript.
+function colorRow(dHour) {
+  //formating for current task
+  var taskCurrently = moment(currently, "H A");
+  //formating for an entry for a later task
+  var taskLater = moment(dHour, "H A");
+  //if statement loop for color coating linked in the CSS document
+  if (taskCurrently.isBefore(taskLater) === true) {
+		return "future";
+	} else if (taskCurrently.isAfter(taskLater) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
+}
 
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  //for save button
-  addEventListener
-  //Click Event will be related to saving local storage.
-  
+//Save button 
+$(".saveBtn").on("click", function() {
+	var slotID = parseInt(
+		$(this)
+			.closest(".time-slot")
+			.attr("id")
+	);
+	var userTask = $.trim(
+		$(this)
+			.parent()
+			.siblings("textarea")
+			.val()
+	);
+	plannerDay[slotID].event = userTask;
 
-
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  //
-  //append new hour-x IDs for the new time slots
-  //link localStoragesetIem with time ID
-  //
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+	/* Set local storage */
+	localStorage.setItem("dayPlanner", JSON.stringify(plannerDay));
 });
+
